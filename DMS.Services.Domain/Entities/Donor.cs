eb2f6c5fbe35360e2b903dcbd385b51007064093
sunildr3511 <sunil.yuvi@gmail.com>
2022-03-18 05:06:28 +0530

    fixed compiler issues

--- DMS.Services.Domain/Entities/Donor.cs
+++ DMS.Services.Domain/Entities/Donor.cs
@@ -1,7 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace DMS.Services.Domain.Entities
