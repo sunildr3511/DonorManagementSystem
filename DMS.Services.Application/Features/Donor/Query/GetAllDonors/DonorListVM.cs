89c5fd6f5dbb87a6ae8a464009d225954177ca73
sunildr3511 <sunil.yuvi@gmail.com>
2022-03-26 15:46:34 +0530

    added few more properties in the DonorlistVM

--- DMS.Services.Application/Features/Donor/Query/GetAllDonors/DonorListVM.cs
+++ DMS.Services.Application/Features/Donor/Query/GetAllDonors/DonorListVM.cs
@@ -22,7 +22,8 @@ namespace DMS.Services.Application.Features
         //public string Comment { get; set; }
         public DateTime FollowUpDate { get; set; }
         public int DonorType { get; set; }
-
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string ContactNo { get; set; }
         public string Email { get; set; }
         public string Address { get; set; }
