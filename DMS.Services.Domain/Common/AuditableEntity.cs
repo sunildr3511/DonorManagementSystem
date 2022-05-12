155580499b5da4e1b0b95a65363f7aa66b5844c7
sunildr3511 <sunil.yuvi@gmail.com>
2022-05-12 13:47:46 +0530

    addded nullable column in Auditable entity

--- DMS.Services.Domain/Common/AuditableEntity.cs
+++ DMS.Services.Domain/Common/AuditableEntity.cs
@@ -6,11 +6,12 @@ namespace DMS.Services.Domain.Common
 {
     public class AuditableEntity
     {
-        public int CreatedBy { get; set; }
+        public int? CreatedBy { get; set; }
 
         public DateTime? CreatedDateTime { get; set; }
 
-        public int UpdatedBy { get; set; }
+        
+        public int? UpdatedBy { get; set; }
 
         public DateTime? UpdatedDateTime { get; set; }
 
